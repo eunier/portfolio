f2237afc5ae9dfb0ae7f2fa07d6795a6f8474a1e
Yunier Alvarez <yunieralvarez@gmail.com>
2022-01-07 23:32:23 -0500

    styled comp

--- src/core/components/Footer.tsx
+++ src/core/components/Footer.tsx
@@ -2,40 +2,38 @@ import { styled } from '@linaria/react';
 import { useState } from 'react';
 import { socialLinks } from '../data';
 
-const StyledDiv = styled.div`
-  .social-links {
-    column-gap: 1.5rem;
-    display: flex;
-    justify-content: center;
-    margin-bottom: 2rem;
-    margin-top: 2rem;
-  }
+const SocialLinks = styled.div`
+  column-gap: 1.5rem;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2rem;
+  margin-top: 2rem;
+`;
 
-  .socialLink-btn {
-    border-radius: 100%;
-    border-width: 0.2rem;
-    display: grid;
-    height: 4.5rem;
-    place-items: center;
-    width: 4.5rem;
-  }
+const SocialLinkBtn = styled.a`
+  border-radius: 100%;
+  border-width: 0.2rem;
+  display: grid;
+  height: 4.5rem;
+  place-items: center;
+  width: 4.5rem;
+`;
 
-  .social-link-icon {
-    font-size: 2rem;
-  }
+const SocialLinkIcon = styled.i`
+  font-size: 2rem;
+`;
 
-  .copy-right {
-    margin-bottom: 0.5rem;
-    margin-top: 0.5rem;
-    text-align: center;
-  }
+const CopyRight = styled.div`
+  margin-bottom: 0.5rem;
+  margin-top: 0.5rem;
+  text-align: center;
 `;
 
 export const Footer = () => {
   const [year] = useState(new Date().getFullYear());
 
   return (
-    <StyledDiv className="container-fluid">
+    <div className="container-fluid">
       <div className="row text-center text-white bg-secondary bg-opacity-75">
         <div className="col-lg-6 col-md-12 mt-4 mb-4">
           <h2>Location</h2>
@@ -46,34 +44,26 @@ export const Footer = () => {
         <div className="col-lg-6 col-md-12 mt-4 mb-4">
           <h2>On The Web</h2>
 
-          <div className="social-links">
+          <SocialLinks>
             {socialLinks.map((sl, idx) => (
-              <a
-                className="btn btn-outline-light socialLink-btn"
+              <SocialLinkBtn
+                className="btn btn-outline-light"
                 key={idx}
                 href={sl.url}
                 target="_blank"
               >
-                <i className={`social-link-icon bi ${sl.icons}`}></i>
-              </a>
+                <SocialLinkIcon className={`bi ${sl.icons}`}></SocialLinkIcon>
+              </SocialLinkBtn>
             ))}
-          </div>
+          </SocialLinks>
         </div>
       </div>
 
       <div className="row bg-secondary">
         <div className="col">
-          <div className="copy-right">
-            &copy; {year} Yunier Alvarez Portfolio
-          </div>
+          <CopyRight>&copy; {year} Yunier Alvarez Portfolio</CopyRight>
         </div>
       </div>
-      <div className="test1 test2">test</div>
-      <style>{`
-        .test1 + .test2 {
-          background-color: red;
-        }
-      `}</style>
-    </StyledDiv>
+    </div>
   );
 };
