49fc44f73e306db4466406adcc41e3ec8a0606c5
Yunier Alvarez <yunieralvarez@gmail.com>
2022-01-07 22:46:54 -0500

    clean up

--- src/core/components/Footer.tsx
+++ src/core/components/Footer.tsx
@@ -3,33 +3,31 @@ import { useState } from 'react';
 import { socialLinks } from '../data';
 
 const FooterStyles = styled.div`
-  .footer {
-    &__social-links {
-      column-gap: 1.5rem;
-      display: flex;
-      justify-content: center;
-      margin-bottom: 2rem;
-      margin-top: 2rem;
-    }
+  .social-links {
+    column-gap: 1.5rem;
+    display: flex;
+    justify-content: center;
+    margin-bottom: 2rem;
+    margin-top: 2rem;
+  }
 
-    &__socialLink-btn {
-      border-radius: 100%;
-      border-width: 0.2rem;
-      display: grid;
-      height: 4.5rem;
-      place-items: center;
-      width: 4.5rem;
-    }
+  .socialLink-btn {
+    border-radius: 100%;
+    border-width: 0.2rem;
+    display: grid;
+    height: 4.5rem;
+    place-items: center;
+    width: 4.5rem;
+  }
 
-    &__social-link-icon {
-      font-size: 2rem;
-    }
+  .social-link-icon {
+    font-size: 2rem;
+  }
 
-    &__copy-right {
-      margin-bottom: 0.5rem;
-      margin-top: 0.5rem;
-      text-align: center;
-    }
+  .copy-right {
+    margin-bottom: 0.5rem;
+    margin-top: 0.5rem;
+    text-align: center;
   }
 `;
 
@@ -48,15 +46,15 @@ export const Footer = () => {
         <div className="col-lg-6 col-md-12 mt-4 mb-4">
           <h2>On The Web</h2>
 
-          <div className="footer__social-links">
+          <div className="social-links">
             {socialLinks.map((sl, idx) => (
               <a
-                className="btn btn-outline-light footer__socialLink-btn"
+                className="btn btn-outline-light socialLink-btn"
                 key={idx}
                 href={sl.url}
                 target="_blank"
               >
-                <i className={`footer__social-link-icon bi ${sl.icons}`}></i>
+                <i className={`social-link-icon bi ${sl.icons}`}></i>
               </a>
             ))}
           </div>
@@ -65,7 +63,7 @@ export const Footer = () => {
 
       <div className="row bg-secondary">
         <div className="col">
-          <div className="footer__copy-right">
+          <div className="copy-right">
             &copy; {year} Yunier Alvarez Portfolio
           </div>
         </div>
