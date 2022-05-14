1c3646a97214422bc4b632cbe77356dcb327d729
Yunier Alvarez <yunieralvarez@gmail.com>
2022-05-14 09:11:19 -0400

    format

--- src/features/portfolio/components/ProjectCard.tsx
+++ src/features/portfolio/components/ProjectCard.tsx
@@ -24,18 +24,16 @@ const ProjectCardStyles = styled.div`
   }
 `;
 
-export const ProjectCard = (props: ProjectCardProps) => {
-  return (
-    <ProjectCardStyles className={classNames('card', props.className)}>
-      <div className="card-img-top d-flex justify-content-center">
-        <i className={classNames('bi', props.icon, 'project-card__icon')}></i>
-      </div>
+export const ProjectCard = (props: ProjectCardProps) => (
+  <ProjectCardStyles className={classNames('card', props.className)}>
+    <div className="card-img-top d-flex justify-content-center">
+      <i className={classNames('bi', props.icon, 'project-card__icon')}></i>
+    </div>
 
-      <div className="card-body">
-        <h5 className="card-title project-card__title-wrapper">
-          <div className="project-card__title">{props.title}</div>
-        </h5>
-      </div>
-    </ProjectCardStyles>
-  );
-};
+    <div className="card-body">
+      <h5 className="card-title project-card__title-wrapper">
+        <div className="project-card__title">{props.title}</div>
+      </h5>
+    </div>
+  </ProjectCardStyles>
+);
