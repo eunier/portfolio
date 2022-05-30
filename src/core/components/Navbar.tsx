1beb416ccf9f7912eb0131da4e09c5b82040ea4e
Yunier Alvarez <yunieralvarez@gmail.com>
2022-05-29 22:43:05 -0400

    icons

--- src/core/components/Navbar.tsx
+++ src/core/components/Navbar.tsx
@@ -6,14 +6,18 @@ type NavLink = {
   icon: string;
 };
 
+const { VITE_ASSETS_URL } = import.meta.env as ImportMetaEnv & {
+  VITE_ASSETS_URL: string;
+};
+
 const navLinkNames: NavLink[] = [
   {
     name: 'home',
-    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/><path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/></svg>',
+    icon: `${VITE_ASSETS_URL}/home.svg`,
   },
   {
     name: 'portfolio',
-    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-briefcase" viewBox="0 0 16 16"><path d="M6.5 1A1.5 1.5 0 0 0 5 2.5V3H1.5A1.5 1.5 0 0 0 0 4.5v8A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-8A1.5 1.5 0 0 0 14.5 3H11v-.5A1.5 1.5 0 0 0 9.5 1h-3zm0 1h3a.5.5 0 0 1 .5.5V3H6v-.5a.5.5 0 0 1 .5-.5zm1.886 6.914L15 7.151V12.5a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5V7.15l6.614 1.764a1.5 1.5 0 0 0 .772 0zM1.5 4h13a.5.5 0 0 1 .5.5v1.616L8.129 7.948a.5.5 0 0 1-.258 0L1 6.116V4.5a.5.5 0 0 1 .5-.5z"/></svg>',
+    icon: `${VITE_ASSETS_URL}/briefcase.svg`,
   },
   // {
   //   name: 'about',
@@ -21,7 +25,7 @@ const navLinkNames: NavLink[] = [
   // },
   {
     name: 'contact',
-    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16"><path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/></svg>',
+    icon: `${VITE_ASSETS_URL}/envelope.svg`,
   },
 ];
 
@@ -61,7 +65,8 @@ export const Navbar = () => {
                   aria-current={idx === activeNavLinkIdx ? 'page' : undefined}
                 >
                   {/* <i className={`bi ${el.icon} pe-2`}></i> */}
-                  <img src={`data:image/svg+xml;utf8,${el.icon}`} />
+                  {/* <img src={`data:image/svg+xml;utf8,${el.icon}`} /> */}
+                  <img src={el.icon}></img>
                   {el.name}
                 </a>
               </li>
