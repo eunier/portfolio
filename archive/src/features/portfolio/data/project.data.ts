36cdd097dac7bd45588b5cad37da52de3b18156a
Yunier Alvarez <yunieralvarez@gmail.com>
2024-01-14 17:16:46 -0500

    add cgit

--- archive/src/features/portfolio/data/project.data.ts
+++ archive/src/features/portfolio/data/project.data.ts
@@ -4,10 +4,20 @@ import devopsDashboardMediaSrc from '../../../assets/devops-dashboard-demo.gif';
 import devConnectorMediaSrc from '../../../assets/dev-connector.png';
 import randomPasswordGeneratorMediaSrc from '../../../assets/random-password-generator.png';
 import weatherAppMediaSrc from '../../../assets/weather-app.gif';
+import cgitMediaSrc from '../../../assets/cgit.png'
 
 
 
 export const projects: Project[] = [
+  {
+    backgroundColor: 'bg-danger',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-git" viewBox="0 0 16 16"><path d="M15.698 7.287 8.712.302a1.03 1.03 0 0 0-1.457 0l-1.45 1.45 1.84 1.84a1.223 1.223 0 0 1 1.55 1.56l1.773 1.774a1.224 1.224 0 0 1 1.267 2.025 1.226 1.226 0 0 1-2.002-1.334L8.58 5.963v4.353a1.226 1.226 0 1 1-1.008-.036V5.887a1.226 1.226 0 0 1-.666-1.608L5.093 2.465l-4.79 4.79a1.03 1.03 0 0 0 0 1.457l6.986 6.986a1.03 1.03 0 0 0 1.457 0l6.953-6.953a1.03 1.03 0 0 0 0-1.457"/></svg>',
+    mediaSrc: cgitMediaSrc,
+    title: 'cgit',
+    description:
+      " `cgit` that wraps `git clone` for organizing project folders in a reverse domain-like structure. Built with Rust.",
+    sourceCodeUrl: 'https://gitlab.com/yunieralvarez/cgit',
+  },
   {
     backgroundColor: 'bg-warning',
     icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-joystick" viewBox="0 0 16 16"><path d="M10 2a2 2 0 0 1-1.5 1.937v5.087c.863.083 1.5.377 1.5.726 0 .414-.895.75-2 .75s-2-.336-2-.75c0-.35.637-.643 1.5-.726V3.937A2 2 0 1 1 10 2"/> <path d="M0 9.665v1.717a1 1 0 0 0 .553.894l6.553 3.277a2 2 0 0 0 1.788 0l6.553-3.277a1 1 0 0 0 .553-.894V9.665c0-.1-.06-.19-.152-.23L9.5 6.715v.993l5.227 2.178a.125.125 0 0 1 .001.23l-5.94 2.546a2 2 0 0 1-1.576 0l-5.94-2.546a.125.125 0 0 1 .001-.23L6.5 7.708l-.013-.988L.152 9.435a.25.25 0 0 0-.152.23"/></svg>',
